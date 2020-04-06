9da7cbf59b232228bffbcee250a0d6812cb374cb
Anderson.Bellini <anderson.bellini@castgroup.com.br>
2020-04-06 09:12:19 -0300

    finish migrations Person using evolve

--- RestWithAspNet/Model/Person.cs
+++ RestWithAspNet/Model/Person.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace RestWithASPNETUdemy.Model
 {
+    [Table("persons")]
     public class Person
     {
         public long? Id { get; set; }
